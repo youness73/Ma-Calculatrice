e905d3a925aa61ed3134e301a23e352988fab303
fullstack-khamlichi <114249953+fullstack-khamlichi@users.noreply.github.com>
2023-10-09 16:15:23 +0100

    1st commit: Android Calculator App

---
+++ app/src/main/java/ma/ensaf/ma_calculatrice/MainActivity.java
@@ -0,0 +1,14 @@
+package ma.ensaf.ma_calculatrice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
